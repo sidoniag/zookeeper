eed5752be9b4df39069101485e3045150a2af6f5
idonia <sidoniag@gmail.com>
2020-07-28 11:08:22 -0500

    add get and listen

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 // instantiate server, tell it to listen for requests 
 const app = express();
+// add the route
+app.get('/api/animals', (req, res) => {
+  res.json(animals);
+});
 // make server listen
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+// front-end request for data
+const { animals } = require('./data/animals');
