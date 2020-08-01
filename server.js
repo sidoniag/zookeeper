23f384fd6ef8befc99aae73ab7017fabbab0a8ea
idonia <sidoniag@gmail.com>
2020-07-31 21:47:49 -0500

    complete data handling

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
-
-const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -59,11 +59,31 @@ function findById(id, animalsArray) {
 
 // accept POST value add to array
 function createNewAnimal(body, animalsArray) {
-  console.log(body);
-  // our function's main code will go here!
-
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
   // return finished code to post route for response
-  return body;
+  return animal;
+}
+
+// function to validate animal data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
 }
 
 // GET route
@@ -91,7 +111,15 @@ app.post('/api/animals', (req, res) => {
   // req.body is where our incoming content will be
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+  // add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
   res.json(req.body);
+  }
 });
 
 // make server listen
